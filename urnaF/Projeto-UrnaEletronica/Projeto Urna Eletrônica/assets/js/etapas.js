3c0b3cf57b380e1e2d6af406ddf18030883ec463
Thiago <122835095+Bladhit@users.noreply.github.com>
2023-01-24 20:14:30 -0300

    Add files via upload

---
+++ urnaF/Projeto-UrnaEletronica/Projeto Urna Eletrônica/assets/js/etapas.js
@@ -0,0 +1,65 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: "01",
+                nome: 'Darth Vader',
+                partido: 'Sife',
+                fotos: [
+                    {URL: 'assets/images/2.png', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/1.png', legenda: 'VICE', small: true}
+                ]
+            },
+            {
+                numero: "02",
+                nome: 'Kratos',
+                partido: 'Valhala',
+                fotos: [
+                    {URL: 'assets/images/Kratos.jpg', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/Sonic.jpg', legenda: 'VICE', small: true}
+                ]
+            },
+            {
+                numero: "03",
+                nome: 'Shrek',
+                partido: 'Distante',
+                fotos: [
+                    {URL: 'assets/images/Shrek.jpg', legenda: 'PRESIDENTE'},
+                    {URL: 'assets/images/Dr.jpg', legenda: 'VICE', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: "10",
+                nome: 'Po',
+                partido: 'Dragão',
+                fotos: [
+                    {URL: 'assets/images/Po.jpg', legenda: 'GOVERNADOR'}
+                ]
+            },
+            {
+                numero: "11",
+                nome: 'Pericles',
+                partido: 'Freio',
+                fotos: [
+                    {URL: 'assets/images/Pericles.jpg', legenda: 'GOVERNADOR'}
+                ]
+            },
+            {
+                numero: "12",
+                nome: 'Scream',
+                partido: 'Panico',
+                fotos: [
+                    {URL: 'assets/images/Scream.jpg', legenda: 'GOVERNADOR'}
+                ]
+            },
+        ]
+    },
+]
